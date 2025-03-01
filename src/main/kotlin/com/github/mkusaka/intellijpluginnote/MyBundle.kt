32cb77b51b1f7bf0a9b884517868034893b45b14
GitHub Action <action@github.com>
2025-03-01 17:15:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/mkusaka/intellijpluginnote/MyBundle.kt
+++ src/main/kotlin/com/github/mkusaka/intellijpluginnote/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mkusaka.intellijpluginnote
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mkusaka/intellijpluginnote/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/mkusaka/intellijpluginnote/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.mkusaka.intellijpluginnote.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
